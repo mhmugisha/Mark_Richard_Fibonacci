6bb54ebf2553cf1bb16d0fc4a7c326fbbc76b2be
Mark Mugisha <mhmugisha@gmail.com>
2023-02-21 23:42:31 +0300

    Mark and Richard Fibonacci Series Assignment

--- test/test.js
+++ test/test.js
@@ -5,9 +5,9 @@ const n = 6;
 
 describe('Calculate the Fibonacci series of number', () => {
   it(`Function Fibonacci series of ${n}`, () => {
-    const result = fibo.fibFunction(n);
+    const resultArray = fibo.fibFunction(n);
     const testArray = [0, 1, 1, 2, 3, 5];
 
-    assert.equal(result.toString(), testArray.toString());
+    assert.equal(resultArray.toString(), testArray.toString());
   });
 });
